2174718cdd432c54a50f105f43173528b0683ee2
Alexander <alexkorpol1964@gmail.com>
2023-05-05 09:58:25 +0300

    Clues for Refrences

--- src/js/api-key.js
+++ src/js/api-key.js
@@ -1,3 +1,12 @@
 const KEY = '0d9ddfeb4636025259fcaee6725b8ad3';
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export { KEY };
+  
+//  Підказка для посилань з ТЗ
+// https://api.themoviedb.org/3/trending/ - Трендові фільми дня/тижня
+// https://api.themoviedb.org/3/movie/upcoming/ - Нові фільми
+// https://api.themoviedb.org/3/search/movie/ - Фільми за ключовим словом
+// https://api.themoviedb.org/3/movie/ - Детальна інформація про фільм
+// https://api.themoviedb.org/3/movie/ - Повна інформація про можливий трейлер фільма на YouTube
+// https://api.themoviedb.org/3/genre/movie/list - Перелік жанрів
+// https://api.themoviedb.org/3/configuration/countries - Перелік країн
