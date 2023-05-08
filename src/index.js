8b920af089f637f65ddd7a8c15e6fd5a5959bbc9
mary-chery <cherkashina.maryna@gmail.com>
2023-05-08 23:19:46 +0300

    fix index js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './js/header';
 import './js/mobile-menu';
 import './js/home';
 import './js/hero';
-import './js/catalog';
 import './js/render_footer_modal';
 import './js/scroll-to-top';
+// import './js/catalog';
