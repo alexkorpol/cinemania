914b5d6deee8d3fc9bbd069dce74d34b0807b3e6
Alexander <116386768+alexkorpol@users.noreply.github.com>
2023-05-08 23:50:45 +0300

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './js/header';
-import './js/mobile-menu';
-import './js/home';
-import './js/hero';
-import './js/render_footer_modal';
-import './js/scroll-to-top';
-// import './js/catalog';
