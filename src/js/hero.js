0195fb90ef4c3e3a35c7691e83554f443666dea4
Artemon Chik <zagrousaspear@gmail.com>
2023-05-09 19:29:43 +0200

    some fixes home_weekly

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,4 +1,4 @@
-import { KEY } from "./api-key";
+import { KEY } from './api-key';
 
 const BASE_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${KEY}&page=1`;
 const IMG_URL = 'https://image.tmdb.org/t/p/original/';
@@ -6,71 +6,69 @@ const IMG_URL = 'https://image.tmdb.org/t/p/original/';
 fetchMovieOfTheDay();
 
 function fetchMovieOfTheDay() {
-    return fetch(BASE_URL)
-      .then((response) => response.json())
-      .then((data) => {
+  return fetch(BASE_URL)
+    .then(response => response.json())
+    .then(data => {
       makeMovieOfTheDay(data);
-      }).catch(error => console.log(error))
-  }
+    })
+    .catch(error => console.log(error));
+}
 
-  const heroEl = document.body.querySelector(".hero");
-  const heroContainer = document.body.querySelector('.hero__container');
-  const heroFilm = document.body.querySelector('.hero__with-film');
+const heroEl = document.body.querySelector('.hero');
+const heroContainer = document.body.querySelector('.hero__container');
+const heroFilm = document.body.querySelector('.hero__with-film');
 
-  function makeMovieOfTheDay(data) {
-    try {
-      if (data) {
-          console.log(data);
-          const valueOfElements = data.results.length - 1
-          console.log(valueOfElements);
-  
-          const movieNumber = Math.round(getRandomNumber(0, valueOfElements));
-          console.log(movieNumber);
-  
-          const film = data.results[movieNumber];
-          console.log(film);
+function makeMovieOfTheDay(data) {
+  try {
+    if (data) {
+      console.log(data);
+      const valueOfElements = data.results.length - 1;
+      console.log(valueOfElements);
 
-          heroContainer.innerHTML = '';
-          makeMarkup(film)
-        } 
-    } catch (error) {
-        console.log(error);
-        heroEl.classList.remove("hero__with-film");
-        heroEl.classList.add("hero__default");
+      const movieNumber = Math.round(getRandomNumber(0, valueOfElements));
+      console.log(movieNumber);
+
+      const film = data.results[movieNumber];
+      console.log(film);
+
+      heroContainer.innerHTML = '';
+      makeMarkup(film);
     }
+  } catch (error) {
+    console.log(error);
+    heroEl.classList.remove('hero__with-film');
+    heroEl.classList.add('hero__default');
   }
-  
-  function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-  
-  function makeMarkup(film) {
-    heroContainer.innerHTML = `<div class="hero__box">
+}
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function makeMarkup(film) {
+  heroContainer.innerHTML = `<div class="hero__box">
     <h2 class="hero__title">${film.title}</h2>
     <p class="rating__value">${film.vote_average}</p>
     <p class="hero__overview">${film.overview}</p>
     <button class="hero__btn ">Watch trailer</button></div>
     <div class="hero__background"></div>
-    `
+    `;
 
-    
-    window.addEventListener('load', setHeroBackground(film));
-    window.addEventListener('resize', setHeroBackground(film));
-  }
-  
+  window.addEventListener('load', setHeroBackground(film));
+  window.addEventListener('resize', setHeroBackground(film));
+}
 
-  function setHeroBackground(film) {
-    const windowWidth = window.innerWidth;
-  
-    
-    if (windowWidth <= 768) {
-      heroFilm.style.backgroundImage = `linear-gradient(87.8deg, #0e0e0e 15.61%, rgba(14, 14, 14, 0) 60.39%), url('${IMG_URL}${film.backdrop_path}')`;
-    } else if(windowWidth >= 769) {
-      heroFilm.style.backgroundImage = `url('${IMG_URL}${film.backdrop_path}')`;
-    } else if(windowWidth >= 1200) {
-      heroFilm.style.backgroundImage = ` url('${IMG_URL}${film.backdrop_path}')`;
-    }
+function setHeroBackground(film) {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 768) {
+    heroFilm.style.backgroundImage = `linear-gradient(87.8deg, #0e0e0e 15.61%, rgba(14, 14, 14, 0) 60.39%), url('${IMG_URL}${film.backdrop_path}')`;
+  } else if (windowWidth >= 769) {
+    heroFilm.style.backgroundImage = `url('${IMG_URL}${film.backdrop_path}')`;
+  } else if (windowWidth >= 1200) {
+    heroFilm.style.backgroundImage = ` url('${IMG_URL}${film.backdrop_path}')`;
   }
+}
 
 // ================== rating ==================
 
