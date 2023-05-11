fd994d99281e104c7b75aab99250aea4025a22ef
Tonya-Pronina <darkpixus@gmail.com>
2023-05-11 17:25:22 +0300

    catalog fix

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,45 +1,8 @@
 import axios from 'axios';
 import { KEY } from './api-key';
-import { renderModal } from './global-modal';
 import Pagination from 'tui-pagination';
-import { renderModal } from './global-modal';
-// import { getDetailFilm } from './home-weekly';
+import {emptyStar, fullStar, halfStar } from './star';
 
-
-const emptyStar = `<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
-<defs>
-<linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`;
-
-const fullStar = `<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="rgba(248, 65, 25, 1)" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
-<defs>
-<linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`;
-
-const halfStar = `<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_148_6991)" stroke-linejoin="round"/>
-<path d="M9 1.6875V12.7969L4.14844 16.3125L6.04688 10.6875L1.125 7.3125H7.17188L9 1.6875Z" fill="url(#paint1_linear_148_6991)"/>
-<defs>
-<linearGradient id="paint0_linear_148_6991" x1="3.04877" y1="2.73251" x2="13.478" y2="16.7124" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-<linearGradient id="paint1_linear_148_6991" x1="2.08688" y1="2.73251" x2="12.1506" y2="9.47748" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`;
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const TREND_URL = `${BASE_URL}/trending/movie/week`;
@@ -202,7 +165,7 @@ async function createListMarkup(data) {
             <h2 class='cards-list__title'>${title}</h2>
             <div class='cards-list_second_line'>
               <div class='cards-list__text'>
-                <p>${slicedGenres} | ${release_date}</p>
+                <p>${genre_ids} | ${release_date}</p>
               </div>
                <div class='star-rate'>
             ${ratingStars}
@@ -338,7 +301,6 @@ cards.addEventListener('click', async evt => {
   modal.classList.remove('is-hidden');
   const id = evt.target.dataset.id;
   const movie = await getDetailFilm(id);
-  renderModal(movie);
   closeButton.addEventListener('click', () => {
     modal.classList.add('is-hidden');
     modalPoster.innerHTML = '';
